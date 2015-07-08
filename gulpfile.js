ba8d28eb9aabd2f0d856c2637a3d5cc32bc58987
Kevin Ruscoe <kevin@popcreative.co.uk>
2015-07-08 18:01:06 +0100

    add image compression task

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,24 @@ concat = require('gulp-concat'),
 uglify = require('gulp-uglify'),
 minifyCSS = require('gulp-minify-css'),
 sourcemaps = require('gulp-sourcemaps'),
+imagemin = require('gulp-imagemin'),
+pngquant = require('imagemin-pngquant'),
 browserSync = require('browser-sync').create();
 
 var settings = {
-	base_input: "assets/",
-	base_output: "public/assets/",
+	root_paths: {
+		input: "assets/",
+		output: "public/assets/",
+	},
 	server_path: "http://255.255.255.255/folder/public"
+}
+
+var images = {
+	root_paths: {
+		input: "img/",
+		output: "img/"
+	},
+	watch: "**/*"
 }
 
 var javascript = {
@@ -20,8 +32,8 @@ var javascript = {
 	},
 	merge: [
 		'bower_components/jquery/dist/jquery.js',
-		"assets/js/**/*.js",
-		"assets/js/app.js"
+		settings.root_paths.input + "js/**/*.js",
+		settings.root_paths.input + "js/app.js"
 	],
 	as: "app",
 	watch: [
@@ -40,10 +52,11 @@ var scss = {
 	]
 }
 
-
+// TODO: dont listen for img/* changes
 var reloader = {
 	what: settings.server_path,
 	when: [
+		"!public/img/*",
 		"public/**/*"
 	]
 }
@@ -52,40 +65,52 @@ var copy = {
 	paths: [
 		{
 			from: './bower_components/bootstrap-sass-official/assets/fonts/**/*',
-			to: 'public/assets/fonts'
+			to: settings.root_paths.output + 'fonts'
 		},
 		{
 			from: './bower_components/fontawesome/fonts/**',
-			to: 'public/assets/fonts/fontawesome'
+			to: settings.root_paths.output + 'fonts/fontawesome'
 		},
 	]
 }
 
 // Core 
 gulp.task('sass', function () {
-	gulp.src(settings.base_input + scss.root_paths.input + scss.as + ".scss")
-	.pipe(sass({errLogToConsole: true}))
-	.pipe(concat(scss.as + ".css"))
-	.pipe(minifyCSS())
-	.pipe(sourcemaps.init())
-	.pipe(sourcemaps.write("map"))
-	.pipe(gulp.dest(settings.base_output + scss.root_paths.output ))
+	return gulp.src(settings.root_paths.input + scss.root_paths.input + scss.as + ".scss")
+		.pipe(sass({errLogToConsole: true}))
+		.pipe(concat(scss.as + ".css"))
+		.pipe(minifyCSS())
+		.pipe(sourcemaps.init())
+		.pipe(sourcemaps.write("map"))
+		.pipe(gulp.dest(settings.root_paths.output + scss.root_paths.output ))
 });
 
 gulp.task('javascript', function () {
-	gulp.src(javascript.merge)
-	.pipe(concat(javascript.as + ".min.js"))
-	.pipe(uglify())
-	.pipe(sourcemaps.init())
-	.pipe(sourcemaps.write("map"))
-	.pipe(gulp.dest(settings.base_output + javascript.root_paths.output ))
+	return gulp.src(javascript.merge)
+		.pipe(concat(javascript.as + ".min.js"))
+		.pipe(uglify())
+		.pipe(sourcemaps.init())
+		.pipe(sourcemaps.write("map"))
+		.pipe(gulp.dest(settings.root_paths.output + javascript.root_paths.output ))
+});
+
+// TODO: clear output folder before compression
+gulp.task('images', function () {
+	return gulp.src(settings.root_paths.input + images.root_paths.input + images.watch)
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest(settings.root_paths.output + images.root_paths.output ));
 });
 
 
 gulp.task('watch', function () {
 	
-	gulp.watch( settings.base_input + scss.root_paths.input + scss.watch, ['sass']);
-	gulp.watch( settings.base_input + javascript.root_paths.input + javascript.watch, ['javascript']);
+	gulp.watch( settings.root_paths.input + scss.root_paths.input + scss.watch, ['sass']);
+	gulp.watch( settings.root_paths.input + javascript.root_paths.input + javascript.watch, ['javascript']);
+	gulp.watch( settings.root_paths.input + images.root_paths.input + images.watch, ['images']);
 	
 	gulp.start(['start-browser-sync-server', 'default']);
 	gulp.watch( reloader.when ).on('change', browserSync.reload );
@@ -106,4 +131,4 @@ gulp.task('start-browser-sync-server', function () {
 	});
 });
 
-gulp.task('default', ['sass', 'javascript']);
+gulp.task('default', ['sass', 'javascript', 'images']);
